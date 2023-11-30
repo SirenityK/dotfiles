351c77aa7423e0ffbe7367b3539ab4d2e9a0bf89
SirenityK <103223321+SirenityK@users.noreply.github.com>
2023-11-29 20:52:26 -0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,22 @@
+BASEDIR=`dirname "$0"`
+
+# Copy all repo files to home directory, subfolders are copied recursively
+cp -r $BASEDIR/. ~
+
+sudo pacman -S lsd bat zsh zsh-completions neovim thefuck kitty wget tmux --noconfirm
+
+bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+
+~/miniconda3/bin/conda init zsh
+
+mkdir ~/.local/share/fonts
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -o '~/.local/share/fonts/MesloLGS NF Regular.ttf'
+
+cat strings.txt | tee ~/.zshrc
+
+exec zsh
