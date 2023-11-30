85971cfc61ebddbd4488fb0734938fe62f277121
SirenityK <103223321+SirenityK@users.noreply.github.com>
2023-11-29 21:27:17 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,18 +4,20 @@ sudo pacman -S lsd bat zsh zsh-completions neovim thefuck kitty wget tmux --noco
 
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
-mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -rf ~/miniconda3/miniconda.sh
+#mkdir -p ~/miniconda3
+#wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+#bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+#rm -rf ~/miniconda3/miniconda.sh
 
-~/miniconda3/bin/conda init zsh
+#~/miniconda3/bin/conda init zsh
 
 mkdir ~/.local/share/fonts
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -o '~/.local/share/fonts/MesloLGS NF Regular.ttf'
 
 # Copy all repo files to home directory, subfolders are copied recursively
-cp -r $BASEDIR/.p10k.zsh $BASEDIR/.xprofile $BASEDIR/.config
+cp $BASEDIR/.p10k.zsh ~
+cp $BASEDIR/.xprofile ~
+cp -r $BASEDIR/.config ~
 
-cat .zshrc >> ~/.zshrc
-cat strings.txt >> ~/.zshrc
+cat $BASEDIR/.zshrc >> ~/.zshrc
+cat $BASEDIR/strings.txt >> ~/.zshrc
