c2e1a7febb44b04d7edd16c3a8cf0351632cf4df
SirenityK <eldarielmario@gmail.com>
2023-12-15 00:22:15 -0800

    more bug fixes

--- run.sh
+++ run.sh
@@ -7,17 +7,22 @@ read -p 'Install python virtual environment? [textToConfirm]: ' install_python
 if [ -n $install_python ]; then
     venv=python-virtualenv
     dvenv=python3-virtualenv
+    tvenv=python
 fi
 
 # detect if termux
 if [ -d '/data/data/com.termux' ]; then
     TERMUX=true
     echo 'Termux detected'
-    pkg install lsd bat zsh zsh-completions neovim wget tmux $dvenv -y
+    pkg update -y
+    pkg upgrade -y
+    pkg install lsd bat zsh zsh-completions neovim wget tmux $tvenv -y
 
 # detect if debian
 elif [ -f '/etc/debian_version' ]; then
     echo 'Debian detected'
+    apt update -y
+    apt upgrade -y
     sudo apt install lsd bat zsh zsh-completions neovim thefuck kitty wget tmux $dvenv -y
     mkdir ~/.local/share/fonts
     wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P ~/.local/share/fonts/
@@ -25,7 +30,7 @@ elif [ -f '/etc/debian_version' ]; then
 # detect if arch
 elif [ -f '/etc/arch-release' ]; then
     echo 'Arch detected'
-    sudo pacman -Sv lsd bat zsh zsh-completions neovim thefuck kitty wget tmux $venv --noconfirm
+    sudo pacman -Syuv lsd bat zsh zsh-completions neovim thefuck kitty wget tmux $venv --noconfirm
     mkdir ~/.local/share/fonts
     wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P ~/.local/share/fonts/
 fi
@@ -45,7 +50,7 @@ cat $BASEDIR/.zshrc >> ~/.zshrc
 if [ -n $install_python ]; then
     python3 -m pip install -U pip wheel setuptools virtualenv
     virtualenv ~/.venv
-    echo 'source ~/.venv/bin/activate' >> .zshrc
+    echo 'source ~/.venv/bin/activate' >> ~/.zshrc
     
     read -p 'Install life-elemental python packages? [textToConfirm]: ' pip
     if [ -n $pip ]; then
