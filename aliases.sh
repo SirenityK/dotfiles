883cc522c8dec7d0d5949ce63c4ae776b7481b3e
SirenityK <eldarielmario@gmail.com>
2025-03-31 12:44:12 -0700

    new aliases

--- aliases.sh
+++ aliases.sh
@@ -4,10 +4,13 @@ alias c=clear
 alias ls=lsd
 alias ip='ip --color=auto'
 alias grep='rg'
+alias clone='git clone'
+alias gclone='git clone https://github.com/'
 alias pull='git pull'
 alias push='git push'
 alias commit='git commit'
 alias start='xdg-open &> /dev/null'
+alias rsync="rsync --progress --filter=':- .gitignore' -aW"
 alias sudo='sudo '
 alias ..='cd ..'
 alias ...='cd ../..'
